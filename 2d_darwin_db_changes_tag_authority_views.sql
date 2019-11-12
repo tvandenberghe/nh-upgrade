4eeecf6fcdf5ec16b999f28a894df74664e35822
Franck Theeten <franck.theeten@africamuseum.be>
2019-11-12 18:24:50 +0100

    set darwin2 user manager of schema ipt

--- 2d_darwin_db_changes_tag_authority_views.sql
+++ 2d_darwin_db_changes_tag_authority_views.sql
@@ -1,4 +1,5 @@
 set search_path to darwin2,public;
+alter table tag_tag_authority  owner to darwin2;
 
 CREATE INDEX tag_tag_authority_idx
   ON darwin2.tag_tag_authority
@@ -12,6 +13,8 @@ CREATE UNIQUE INDEX tag_authority_idx
 
 DROP MATERIALIZED VIEW IF EXISTS darwin2.mv_tag_to_country;
 create schema ipt;
+grant all on schema ipt to darwin2;
+
 CREATE MATERIALIZED VIEW ipt.mv_tag_to_country AS
 SELECT t1.gtu_ref,
     t1.tag_group_distinct_ref,
@@ -35,7 +38,7 @@ SELECT t1.gtu_ref,
 WITH DATA;
 
 ALTER TABLE ipt.mv_tag_to_country
-  OWNER TO postgres;
+  OWNER TO darwin2;
 set search_path to darwin2,public;
 
 CREATE INDEX tag_tag_authority_idx
@@ -55,54 +58,60 @@ DROP MATERIALIZED VIEW IF EXISTS darwin2.mv_darwin_ipt_rbins;
 DROP VIEW IF EXISTS darwin2.v_darwin_ipt_rbins;
 
 DROP MATERIALIZED VIEW IF EXISTS darwin2.mv_tag_to_locations;
-CREATE MATERIALIZED VIEW ipt.mv_tag_to_locations AS 
-SELECT DISTINCT 
-	t.id AS tag_identifier,
-	t.gtu_ref AS gtu_identifier,
-	tta.tag_group_distinct_ref AS distinct_tag_identifier,
-	t.tag_value AS original_location,
-	t.group_name_indexed AS original_type,
-	t.sub_group_name_indexed AS original_sub_type,
+CREATE MATERIALIZED VIEW ipt.mv_tag_to_locations
+TABLESPACE pg_default
+AS
+ SELECT DISTINCT t.id AS tag_identifier,
+    t.gtu_ref AS gtu_identifier,
+    tta.tag_group_distinct_ref AS distinct_tag_identifier,
+    t.tag_value AS original_location,
+    t.group_name_indexed AS original_type,
+    t.sub_group_name_indexed AS original_sub_type,
     tcat_gn.gazetteer_type_mapped AS geonames_type_mapped,
     tcat_mrg.gazetteer_type_mapped AS marineregions_type_mapped,
     tcat_gn.priority,
-    case when tcat_gn.gazetteer_type_mapped = 'PCLI' then countries.country_code_gn else ta.code end AS gazetteer_code,
-    case when tcat_gn.gazetteer_type_mapped = 'PCLI' then countries.country_url_gn else ta.url end AS gazetteer_url,
+        CASE
+            WHEN tcat_gn.gazetteer_type_mapped = 'PCLI'::text THEN countries.country_code_gn
+            ELSE ta.code
+        END AS gazetteer_code,
+        CASE
+            WHEN tcat_gn.gazetteer_type_mapped = 'PCLI'::text THEN countries.country_url_gn
+            ELSE ta.url
+        END AS gazetteer_url,
     ta.pref_label AS gazetteer_pref_label,
-    cast (case when tcat_gn.gazetteer_type_mapped = 'PCLI' then countries.country_coord->>'latitude_wgs_84' else coordinates.lower_value end as NUMERIC) as latitude,
-    cast (case when tcat_gn.gazetteer_type_mapped = 'PCLI' then countries.country_coord->>'longitude_wgs_84' else props_lon.lower_value end as NUMERIC) as longitude,
+        CASE
+            WHEN tcat_gn.gazetteer_type_mapped = 'PCLI'::text THEN countries.country_coord ->> 'latitude_wgs_84'::text
+            ELSE prop_coordinates.coords ->> 'latitude_wgs_84'::text
+        END::numeric AS latitude,
+        CASE
+            WHEN tcat_gn.gazetteer_type_mapped = 'PCLI'::text THEN countries.country_coord ->> 'latitude_wgs_84'::text
+            ELSE prop_coordinates.coords ->> 'longitude_wgs_84'::text
+        END::numeric AS longitude,
     countries.country_iso,
-    countries.country_pref_label_gn as country_pref_label
-   FROM darwin2.tag_groups t
-     LEFT JOIN darwin2.gtu ON  t.gtu_ref=gtu.id 
-    
-     LEFT JOIN darwin2.tag_tag_authority tta ON tta.tag_group_distinct_ref = t.tag_group_distinct_ref and t.sub_group_name_indexed::text <> 'country'::text
-     LEFT JOIN darwin2.tag_authority ta ON tta.tag_authority_ref = ta.id
-     LEFT JOIN darwin2.tag_groups_authority_categories tcat_gn on tcat_gn.original_type = t.group_name_indexed and tcat_gn.original_sub_type = t.sub_group_name_indexed and tcat_gn.authority = 'geonames.org'--get the priority of the original terms
-     LEFT JOIN darwin2.tag_groups_authority_categories tcat_mrg on tcat_mrg.original_type = t.group_name_indexed and tcat_mrg.original_sub_type = t.sub_group_name_indexed and tcat_mrg.authority = 'marineregions.org' --get the priority of the original terms
-    
-     LEFT JOIN (
-            SELECT
-            distinct
-            record_id, jsonb_object_agg(property_type, lower_value)
-
-            FROM darwin2.properties props_lat
-            WHERE props_lat.referenced_relation='tag_authority' and 
-            (props_lat.property_type='latitude_wgs_84'
-
-            OR
-             props_lat.property_type='longitude_wgs_84'
-            )
-            group by record_id
-            )
-
-            AS
-            prop_coordinates
-     LEFT JOIN (select distinct gtu_ref, tag_group_distinct_ref, country_iso, country_pref_label_gn, country_code_gn, country_url_gn,country_coord 
-     from ipt.mv_tag_to_country) countries ON countries.gtu_ref = t.gtu_ref
-     ORDER BY t.tag_value
+    countries.country_pref_label_gn AS country_pref_label
+   FROM tag_groups t
+     LEFT JOIN gtu ON t.gtu_ref = gtu.id
+     LEFT JOIN tag_tag_authority tta ON tta.tag_group_distinct_ref = t.tag_group_distinct_ref AND t.sub_group_name_indexed::text <> 'country'::text
+     LEFT JOIN tag_authority ta ON tta.tag_authority_ref = ta.id
+     LEFT JOIN tag_groups_authority_categories tcat_gn ON tcat_gn.original_type::text = t.group_name_indexed::text AND tcat_gn.original_sub_type::text = t.sub_group_name_indexed::text AND tcat_gn.authority = 'geonames.org'::text
+     LEFT JOIN tag_groups_authority_categories tcat_mrg ON tcat_mrg.original_type::text = t.group_name_indexed::text AND tcat_mrg.original_sub_type::text = t.sub_group_name_indexed::text AND tcat_mrg.authority = 'marineregions.org'::text
+     LEFT JOIN ( SELECT DISTINCT props_lat.record_id,
+            jsonb_object_agg(props_lat.property_type, props_lat.lower_value) AS coords
+           FROM properties props_lat
+          WHERE props_lat.referenced_relation::text = 'tag_authority'::text AND (props_lat.property_type::text = 'latitude_wgs_84'::text OR props_lat.property_type::text = 'longitude_wgs_84'::text)
+          GROUP BY props_lat.record_id) prop_coordinates ON ta.id = prop_coordinates.record_id
+     LEFT JOIN ( SELECT DISTINCT mv_tag_to_country.gtu_ref,
+            mv_tag_to_country.tag_group_distinct_ref,
+            mv_tag_to_country.country_iso,
+            mv_tag_to_country.country_pref_label_gn,
+            mv_tag_to_country.country_code_gn,
+            mv_tag_to_country.country_url_gn,
+            mv_tag_to_country.country_coord
+           FROM ipt.mv_tag_to_country) countries ON countries.gtu_ref = t.gtu_ref
 WITH DATA;
 
-ALTER TABLE ipt.mv_tag_to_locations OWNER TO postgres;
+ALTER TABLE ipt.mv_tag_to_locations
+    OWNER TO postgres;
+
 GRANT ALL ON TABLE ipt.mv_tag_to_locations TO postgres;
 GRANT ALL ON TABLE ipt.mv_tag_to_locations TO darwin2;
